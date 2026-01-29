d9ee782e5fff1c781a1ed50f675d30cd34e30e1d
Pravesh-Jamgade <praveshjamgade@gmail.com>
2026-01-29 15:51:02 +0530

    perfect nuca

--- sniper/common/core/memory_subsystem/parametric_dram_directory_msi/nuca_cache.cc
+++ sniper/common/core/memory_subsystem/parametric_dram_directory_msi/nuca_cache.cc
@@ -36,6 +36,7 @@ NucaCache::NucaCache(MemoryManagerBase* memory_manager, ShmemPerfModel* shmem_pe
 
    if (Sim()->getCfg()->getBool("perf_model/nuca/queue_model/enabled"))
    {
+      perfect = Sim()->getCfg()->getBool("perf_model/nuca/cache/perfect");
       String queue_model_type = Sim()->getCfg()->getString("perf_model/nuca/queue_model/type");
       m_queue_model = QueueModel::create("nuca-cache-queue", m_core_id, queue_model_type, m_data_array_bandwidth.getRoundedLatency(8 * m_cache_block_size)); // bytes to bits
    }
