242b57e1c944c8dc8bf851276bb15ee4035ce8a5
alchemist <praveshjamgade@gmail.com>
2026-02-01 15:36:04 +0530

    Track virtualized PTW nested stats

--- sniper/common/core/memory_subsystem/parametric_dram_directory_msi/pwc.h
+++ sniper/common/core/memory_subsystem/parametric_dram_directory_msi/pwc.h
@@ -25,10 +25,6 @@ namespace ParametricDramDirectoryMSI
     UInt64 m_l3_access, m_l3_miss;
     UInt64 m_l2_access, m_l2_miss;
     
-
-
-
-
     core_id_t m_core_id;
     bool perfect;
 
@@ -45,6 +41,7 @@ namespace ParametricDramDirectoryMSI
 
     PWC(String name, String cfgname, core_id_t core_id, UInt32 L1_num_entries, UInt32 L1_associativity, UInt32 L2_num_entries, UInt32 L2_associativity, UInt32 L3_num_entries, UInt32 L3_associativity, ComponentLatency access_latency, ComponentLatency miss_latency, bool perfect);
     PWC::where_t lookup(IntPtr address, SubsecondTime now, bool allocate_on_miss , int level , bool count);
+    PWC::where_t probe(IntPtr address, int level);
     void allocate(IntPtr address, SubsecondTime now, Cache *pwc_cache);
     static const UInt64 HASH_PRIME = 124183;
  
